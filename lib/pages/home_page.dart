3b91cc6e445ea2eb6d5d06c4674d1da8acaf37b7
paul-tes <pauasca8462@gmail.com>
2023-11-08 12:43:04 +0300

    Add Task component

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/utils/todo_tile.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -9,6 +10,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List todoList = [
+    ["Task one wag", false],
+    ["Do exersise task two", false]
+  ];
+
+  // check box tabed
+  void checkBoxChanged(int index) {
+    setState(() {
+      todoList[index][1] = ! todoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +30,17 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("To Do"),
         elevation: 0,
+      ),
+
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: todoList[index][0],
+            taskCompleted: todoList[index][1],
+            onChanged: (value) => checkBoxChanged(index),
+          );
+        },
       ),
     );
   }
