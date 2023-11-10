2e67be9a5789a4498e0255c13fcf8f5af5673f47
paul-tes <pauasca8462@gmail.com>
2023-11-10 18:02:53 +0300

    Add custome colors

---
+++ lib/constants/Colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+const Color myRed = Color(0xFFDA4040);
+
+const Color myBlue = Color(0xFF5F52EE);
+
+const Color myBlack = Color(0xFF3A3A3A);
+
+const Color myGrey = Color(0xFF717171);
+
+const Color myBGColor = Color(0xFFEEEFF5);
